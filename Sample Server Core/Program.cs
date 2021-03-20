be34495e599eb87fa8b0b0807e914e0d6512f99d
Shuu <ghwls328@naver.com>
2021-03-20 22:25:49 +0900

    Initial commit

---
+++ Sample Server Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample_Server_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
