347a8c804bc82033fd5120ccf9b8904d1b14b1ec
Shuu <ghwls328@naver.com>
2021-03-20 23:10:53 +0900

    Create Task
    ThreadPool 처럼 활용하되 옵션을 넣어 유연하게 사용 가능

--- Sample Server Core/Program.cs
+++ Sample Server Core/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Sample_Server_Core
 {
     class Program
     {
+        static void MainThread(object state)
+        {
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine("Hello Thread");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(5, 5);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
+                t.Start();
+            }
+
+            ThreadPool.QueueUserWorkItem(MainThread);
+            //t.Name = "Test Thread";
+            //t.IsBackground = true;
+            //t.Start();
+
+            //Console.WriteLine("Hello World!");
+
+            //t.Join();
+            //Console.WriteLine("Hello World!");
         }
     }
 }
