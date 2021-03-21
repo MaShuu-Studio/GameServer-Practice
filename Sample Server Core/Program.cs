7cee8ecafa94f58545ba6187fe893828af030d41
Shuu <ghwls328@naver.com>
2021-03-22 00:32:53 +0900

    Interlocked

--- Sample Server Core/Program.cs
+++ Sample Server Core/Program.cs
@@ -4,55 +4,37 @@ using System.Threading.Tasks;
 
 namespace Sample_Server_Core
 {
-    // 1. Full Memory Barrier (ASM MFENCE, C# Thread.MemoryBarrier) : Store/Load 둘 다 방지
-    // 2. Store Memory Barrier (ASM SFENCE) : Store만 방지
-    // 3. Load Memory Barrier (ASM LFENCE) : Load만 방지
     class Program
     {
-        static int x = 0;
-        static int y = 0;
-        static int r1 = 0;
-        static int r2 = 0;
+        // 실행이 되거나 안되거나.
+        // Interlocked가 걸려있으면 다른 함수가 진행이 되지 않아 원자성을 보장해줌.
+        // 그만큼 성능면에서 저하.
+
+        static int number = 0;
 
         static void Thread_1()
         {
-            y = 1;
-
-            Thread.MemoryBarrier();
-
-            r1 = x;
+            for (int i = 0; i < 1000000; i++)
+                Interlocked.Increment(ref number);
         }
 
         static void Thread_2()
         {
-            x = 1;
-
-            Thread.MemoryBarrier();
-
-            r2 = y;
+            for (int i = 0; i < 1000000; i++)
+                Interlocked.Decrement(ref number);
         }
 
         static void Main(string[] args)
         {
-            int count = 0;
-            while(true)
-            {
-                x = y = r1 = r2 = 0;
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
 
-                Task t1 = new Task(Thread_1);
-                Task t2 = new Task(Thread_2);
+            t1.Start();
+            t2.Start();
 
-                t1.Start();
-                t2.Start();
+            Task.WaitAll(t1, t2);
 
-                Task.WaitAll(t1, t2);
-
-                if (r1 == 0 && r2 == 0) break;
-
-                count++;
-            }
-
-            Console.WriteLine($"Count = {count}");
+            Console.WriteLine(number);
         }
     }
 }
