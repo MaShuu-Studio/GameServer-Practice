64ec28a633b21bddd09bbe8b69fe0971f82e5f40
Shuu <ghwls328@naver.com>
2021-03-22 18:44:46 +0900

    SpinLock

--- Sample Server Core/Program.cs
+++ Sample Server Core/Program.cs
@@ -4,63 +4,52 @@ using System.Threading.Tasks;
 
 namespace Sample_Server_Core
 {
-    class SessionManager
+    class SpinLock
     {
-        static object _lock = new object();
+        volatile int _locked = 0; 
 
-        public static void Test()
+        public void Acquire()
         {
-            lock (_lock)
+            while (true)
             {
-                UserManager.TestUser();
+                // lock이 풀리기까지 대기
+                /*
+                int origin = Interlocked.Exchange(ref _locked, 1);
+                if (origin == 0) break;
+                */
+                int expected = 0;
+                int desired = 1;
+                if(Interlocked.CompareExchange(ref _locked, desired, expected) == expected) break;
             }
         }
 
-        public static void TestSession()
+        public void Release()
         {
-            lock(_lock)
-            {
-
-            }
-        }
-    }
-    class UserManager
-    {
-        static object _lock = new object();
-
-        public static void Test()
-        {
-            lock (_lock)
-            {
-                SessionManager.TestSession();
-            }
-        }
-
-        public static void TestUser()
-        {
-            lock (_lock)
-            {
-
-            }
+            _locked = 0;
         }
     }
     class Program
     {
         static int number = 0;
+        static SpinLock _lock = new SpinLock();
 
         static void Thread_1()
         {
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < 1000000000; i++)
             {
-                SessionManager.Test();
-            }    
+                _lock.Acquire();
+                number++;
+                _lock.Release();
+            }
         }
 
         static void Thread_2()
         {
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < 1000000000; i++)
             {
-                UserManager.Test();
+                _lock.Acquire();
+                number--;
+                _lock.Release();
             }
         }
 
@@ -72,7 +61,7 @@ namespace Sample_Server_Core
             t1.Start();
             t2.Start();
 
-            Task.WaitAll(t1, t2);
+            Task.WaitAll();
 
             Console.WriteLine(number);
         }
