6f629a78ececd28adeea8916eb376c067180f154
Shuu <ghwls328@naver.com>
2021-03-22 17:58:33 +0900

    DeadLock Example

--- Sample Server Core/Program.cs
+++ Sample Server Core/Program.cs
@@ -4,34 +4,55 @@ using System.Threading.Tasks;
 
 namespace Sample_Server_Core
 {
+    class SessionManager
+    {
+        static object _lock = new object();
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                UserManager.TestUser();
+            }
+        }
+
+        public static void TestSession()
+        {
+            lock(_lock)
+            {
+
+            }
+        }
+    }
+    class UserManager
+    {
+        static object _lock = new object();
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+
+        public static void TestUser()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+    }
     class Program
     {
         static int number = 0;
-        static object _obj = new object();
 
         static void Thread_1()
         {
             for (int i = 0; i < 1000000; i++)
             {
-                try
-                {
-                    // Critical Section에 진입 시도
-                    Monitor.Enter(_obj);
-                    number++;
-                }
-                finally
-                {
-                    // Critical Section에서 나옴
-                    Monitor.Exit(_obj);
-                }
-
-                // 일반적으로 직접적으로 관리하기 힘들기 때문에 try-catch finally를 활용 가능
-                // 다만 이 경우도 불편하기 때문에 lock 구문 활용
-
-                lock (_obj)
-                {
-                    number++;
-                }
+                SessionManager.Test();
             }    
         }
 
@@ -39,9 +60,7 @@ namespace Sample_Server_Core
         {
             for (int i = 0; i < 1000000; i++)
             {
-                Monitor.Enter(_obj);
-                number--;
-                Monitor.Exit(_obj);
+                UserManager.Test();
             }
         }
 
