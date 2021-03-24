9ba0a6ac60a895cedd14e2d07fde67b34f973eaa
Shuu <ghwls328@naver.com>
2021-03-25 01:48:42 +0900

    Thread Local Storage

--- Sample Server Core/Program.cs
+++ Sample Server Core/Program.cs
@@ -6,38 +6,23 @@ namespace Sample_Server_Core
 {
     class Program
     {
-        static volatile int count = 0;
-        static Lock _lock = new Lock();
+        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"My name is {Thread.CurrentThread.ManagedThreadId}"; });
+        // null일 때 입력해준 function이 작동
+        // 만약 함수 없이 메서드에 Value를 직접 변동시켜준다면 같은 쓰레드가 호출되더라도 다시 변경시키는 비효율적인 방식이 됨.
+
+        static void WhoAmI()
+        {
+            bool repeat = ThreadName.IsValueCreated;
+            if (repeat) 
+                Console.WriteLine(ThreadName.Value + " - repeat");
+            else
+                Console.WriteLine(ThreadName.Value);
+        }
         static void Main(string[] args)
         {
-            Task t1 = new Task(delegate ()
-            {
-                for (int i = 0; i < 1000; i++)
-                {
-                    _lock.WriteLock();
-                    _lock.WriteLock();
-                    count++;
-                    _lock.WriteUnLock();
-                    _lock.WriteUnLock();
-                }
-            });
-
-            Task t2 = new Task(delegate ()
-            {
-                for (int i = 0; i < 1000; i++)
-                {
-                    _lock.WriteLock();
-                    count--;
-                    _lock.WriteUnLock();
-                }
-            });
-
-            t1.Start();
-            t2.Start();
-
-            Task.WaitAll(t1, t2);
-
-            Console.WriteLine(count);
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3, 3);
+            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
         }
     }
 }
