6ce9a93b4c15e89da2c0c79f72f032c0481cb281
Shuu <ghwls328@naver.com>
2021-04-03 17:46:31 +0900

    basic session sendasync

--- Sample Server Core/Session.cs
+++ Sample Server Core/Session.cs
@@ -11,6 +11,11 @@ namespace Sample_Server_Core
         Socket _socket;
         int _disconnect = 0;
 
+        SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
+        Queue<byte[]> _sendQueue = new Queue<byte[]>();
+        bool _pending = false;
+
+        object _lock = new object();
         public void Init(Socket socket)
         {
             _socket = socket;
@@ -18,12 +23,23 @@ namespace Sample_Server_Core
             recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
             recvArgs.SetBuffer(new byte[1024], 0, 1024);
 
+            _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
+
             RegisterRecv(recvArgs);
         }
 
         public void Send(byte[] sendBuff)
         {
-            _socket.Send(sendBuff);
+            //_socket.Send(sendBuff);
+            // 보낼 때 등록
+            //_sendArgs.SetBuffer(sendBuff, 0, sendBuff.Length); // 종료가 되지 않았는데 버퍼가 채워질 수 있음.
+            
+            lock (_lock)
+            {
+                _sendQueue.Enqueue(sendBuff);
+                if (_pending == false)
+                    RegisterSend();
+            }
         }
 
         public void Disconnect()
@@ -35,6 +51,43 @@ namespace Sample_Server_Core
         }
 
         #region 네트워크 통신
+
+        void RegisterSend()
+        {
+            _pending = true; // 전송 시 큐 닫음
+            byte[] sendBuff = _sendQueue.Dequeue();
+            _sendArgs.SetBuffer(sendBuff, 0, sendBuff.Length);
+
+            bool pending = _socket.SendAsync(_sendArgs);
+            if (pending == false) OnSendCompleted(null, _sendArgs);
+        }
+
+        void OnSendCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            lock (_lock)
+            {
+                if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+                {
+                    try
+                    {
+                        if (_sendQueue.Count > 0) // 큐에 내용이 아직 남아있다면
+                            RegisterSend(); // 마저 털어냄
+                        else 
+                            _pending = false; // 전송 후 큐 열음
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"OnSendCompleted Failed {e}");
+                    }
+                }
+                else
+                {
+
+                }
+            }
+            
+        }
+
         void RegisterRecv(SocketAsyncEventArgs args)
         {
             bool pending = _socket.ReceiveAsync(args);
