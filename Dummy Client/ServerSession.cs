f9724a295b73121cebb818fed78274ab358789b1
Shuu <ghwls328@naver.com>
2021-04-06 15:25:18 +0900

    abstract packet

--- Dummy Client/ServerSession.cs
+++ Dummy Client/ServerSession.cs
@@ -7,21 +7,57 @@ using Sample_Server_Core;
 
 namespace Dummy_Client
 {
-    public class Packet
+    public abstract class Packet
     {
         public ushort size; // 사이즈를 통해 모든 패킷이 왔는지 확인함.
         public ushort packetId; //어떠한 패킷인지 확인함.
+
+        public abstract ArraySegment<byte> Write();
+        public abstract void Read(ArraySegment<byte> s);
     }
 
     class PlayerInfoReq : Packet
     {
         public long playerId;
-    }
 
-    class PlayerInfoOk : Packet
-    {
-        public int hp;
-        public int atk;
+        public PlayerInfoReq()
+        {
+            packetId = (ushort)PacketID.PlayerInfoReq;
+        }
+        public override void Read(ArraySegment<byte> buffer)
+        {
+            int count = 0;
+
+            //ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
+            count += 2;
+            //ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
+            count += 2;
+
+            // 전송된 패킷에 문제가 있더라도 제대로ㅓ 잘 체크할 수 있도록 해줘야함.
+            playerId = BitConverter.ToInt64(new ReadOnlySpan<byte>(buffer.Array, buffer.Offset + count, buffer.Count - count));
+            count += 8;
+
+        }
+
+        public override ArraySegment<byte> Write()
+        {
+            ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
+            bool success = true;
+            ushort count = 0;
+
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(openSegment.Array, openSegment.Offset + count, openSegment.Count - count), this.packetId);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(openSegment.Array, openSegment.Offset + count, openSegment.Count - count), this.playerId);
+            count += 8;
+
+            this.size = count;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(openSegment.Array, openSegment.Offset, openSegment.Count), size);
+
+            if (!success) return null;
+
+            return SendBufferHelper.Close(size);
+        }
     }
 
     public enum PacketID
@@ -38,24 +74,10 @@ namespace Dummy_Client
 
             try
             {
-                PlayerInfoReq packet = new PlayerInfoReq() { packetId = (ushort)PacketID.PlayerInfoReq, playerId = 1001 };
+                PlayerInfoReq packet = new PlayerInfoReq() { playerId = 1001 };
                 //for (int i = 0; i < 5; i++)
                 {
-                    ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
-                    bool success = true;
-                    ushort count = 0;
-
-                    count += 2;
-                    success &= BitConverter.TryWriteBytes(new Span<byte>(openSegment.Array, openSegment.Offset + count, openSegment.Count - count), packet.packetId);
-                    count += 2;
-                    success &= BitConverter.TryWriteBytes(new Span<byte>(openSegment.Array, openSegment.Offset + count, openSegment.Count - count), packet.playerId);
-                    count += 8;
-
-                    packet.size = count;
-                    success &= BitConverter.TryWriteBytes(new Span<byte>(openSegment.Array, openSegment.Offset, openSegment.Count), packet.size);
-                    
-                    ArraySegment<byte> sendBuff = SendBufferHelper.Close(packet.size);
-
+                    ArraySegment<byte> sendBuff = packet.Write();
                     Send(sendBuff);
                 }
             }
--- Sample Server/ClientSession.cs
+++ Sample Server/ClientSession.cs
@@ -8,21 +8,56 @@ using Sample_Server_Core;
 
 namespace Sample_Server
 {
-    public class Packet
+    public abstract class Packet
     {
         public ushort size; // 사이즈를 통해 모든 패킷이 왔는지 확인함.
         public ushort packetId; //어떠한 패킷인지 확인함.
+
+        public abstract ArraySegment<byte> Write();
+        public abstract void Read(ArraySegment<byte> s);
     }
+
     class PlayerInfoReq : Packet
     {
         public long playerId;
+
+        public PlayerInfoReq()
+        {
+            packetId = (ushort)PacketID.PlayerInfoReq;
+        }
+        public override void Read(ArraySegment<byte> buffer)
+        {
+            int count = 0;
+
+            //ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
+            count += 2;
+            //ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
+            count += 2;
+            playerId = BitConverter.ToInt64(new ReadOnlySpan<byte>(buffer.Array, buffer.Offset + count, buffer.Count - count));
+            count += 8;
+        }
+
+        public override ArraySegment<byte> Write()
+        {
+            ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
+            bool success = true;
+            ushort count = 0;
+
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(openSegment.Array, openSegment.Offset + count, openSegment.Count - count), this.packetId);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(openSegment.Array, openSegment.Offset + count, openSegment.Count - count), this.playerId);
+            count += 8;
+
+            this.size = count;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(openSegment.Array, openSegment.Offset, openSegment.Count), size);
+
+            if (!success) return null;
+
+            return SendBufferHelper.Close(size);
+        }
     }
 
-    class PlayerInfoOk : Packet
-    {
-        public int hp;
-        public int atk;
-    }
 
     public enum PacketID
     {
@@ -74,9 +109,9 @@ namespace Sample_Server
             {
                 case PacketID.PlayerInfoReq:
                     {
-                        long playerId = BitConverter.ToInt64(buffer.Array, buffer.Offset + count);
-                        count += 8;
-                        Console.WriteLine($"PlaerInfoReq : {playerId}");
+                        PlayerInfoReq packet = new PlayerInfoReq();
+                        packet.Read(buffer);
+                        Console.WriteLine($"PlaerInfoReq : {packet.playerId}");
                     }
                     break;
             }
